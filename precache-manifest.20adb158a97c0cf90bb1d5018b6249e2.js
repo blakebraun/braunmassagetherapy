9a7315a8f9adebddad1e8f1dcc506cc19525abc8
Blake Braun <blakebraun@live.com>
2019-02-12 21:07:09 -0700

    Add the right files this time

---
+++ precache-manifest.20adb158a97c0cf90bb1d5018b6249e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0f8eba284f46861e53ff",
+    "url": "./static/js/main.0f8eba28.chunk.js"
+  },
+  {
+    "revision": "2592bbdfe53cb8132483",
+    "url": "./static/js/1.2592bbdf.chunk.js"
+  },
+  {
+    "revision": "0f8eba284f46861e53ff",
+    "url": "./static/css/main.a05cac0d.chunk.css"
+  },
+  {
+    "revision": "bbd369670a55cf8ef1a7133e6bcb3f24",
+    "url": "./index.html"
+  }
+];
